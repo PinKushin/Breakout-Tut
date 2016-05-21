086b2dce5d607da3d8912c56c043651becaeb3ab
John Moore <PinKushin@verizon.net>
2016-05-21 10:12:25 -0400

    finished episode 3

--- Scripts/ball.gd
+++ Scripts/ball.gd
@@ -1,5 +1,8 @@
 extends RigidBody2D
 
+const SPEEDUP = 400
+const MAXSPEED = 300000
+
 func _ready():
 	set_fixed_process(true)
 
@@ -9,3 +12,12 @@ func _fixed_process(delta):
 	for body in bodies:
 		if body.is_in_group("Bricks"):
 			body.queue_free()
+		
+		if body.get_name() == "Paddle":
+			var speed = get_linear_velocity().length()
+			var direction = get_pos() - body.get_node("Ancor").get_global_pos()
+			var velocity = direction.normalized()*min(speed+SPEEDUP*delta, MAXSPEED*delta)
+			set_linear_velocity(velocity)
+
+	if get_pos().y > get_viewport_rect().end.y:
+		queue_free()
